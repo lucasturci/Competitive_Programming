dfa50565d6c68cfa9e9d0610fde68b63d723fbaf
Lucas Turci <lucas.turci@gmail.com>
2020-03-28 21:40:50 -0300

    Add string problems to classical problems list and add lcs of multiple strings solution

--- ClassicalProblems/list.md
+++ ClassicalProblems/list.md
@@ -4,4 +4,15 @@ This is a list of links to classical problems, if you ever need to practice an a
 # Geometry
 
 -  **Minimum circle enclosing all points**
+
 https://atcoder.jp/contests/abc151/tasks/abc151_f
+
+# String
+
+- **Longest Common Substring of 2 Strings**
+    - https://www.codechef.com/problems/SSTORY
+    - https://www.spoj.com/problems/LCS/ ( tight TLE, suffix array doesn't work )
+
+- **Longest Common Substring of K Strings**
+
+https://www.spoj.com/problems/LONGCS/
--- String/SuffixArray/problems/lcs_multiple_strings.cpp
+++ String/SuffixArray/problems/lcs_multiple_strings.cpp
@@ -3,6 +3,7 @@
 
 #define fi first
 #define se second
+#define pb push_back
 
 using namespace std;
 
@@ -15,7 +16,7 @@ vector<int> suffix_array(string & s) {
     int len = 1;
 
     iota(a.begin(), a.end(), 0);
-    sort(a.begin(), a.end(), [=](int a, int b) {
+    sort(a.begin(), a.end(), [&](int a, int b) {
         return s[a] < s[b];
     });
 
@@ -62,13 +63,10 @@ vector<int> build_lcp(vector<int> & a, string & s) {
 
     int sz = 0;
     for(int i = 0; i < n; ++i) {
-        if(pos[i] + 1 == n) {
-            sz = 0;
-            continue;
-        } 
+        if(pos[i] + 1 == n) continue;    
         int j = a[pos[i] + 1];
-        while(i + sz < n and j + sz < n and s[i + sz] == s[j + sz]) sz++;
-        lcp[i] = sz;
+        while(i + sz < n and j + sz < n and s[i + sz] == s[j + sz] and s[i + sz] != '$') sz++;
+        lcp[pos[i]] = sz;
         if(sz) sz--;
     }
 
@@ -121,51 +119,67 @@ struct min_queue {
     }
 };
 
-int n, m;
-int cnt[2];
+void _solve() {
+    int n; cin >> n;
 
-int tot;
-void add(int x) {
-    int p = x > n;
-    if(cnt[p] == 0) tot++;
-    cnt[p]++;
-}
+    vector<int> id, cnt(n, 0);
 
-void remove(int x) {
-    int p = x > n;
-    cnt[p]--;
-    if(cnt[p] == 0) tot--;
-}
-
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    string s, t;
-    cin >> s >> t;
-
-    n = s.size(), m = t.size();
-    string x = s + "$" + t + "$";
+    string x = "";
+    for(int i = 0; i < n; ++i) {
+        string s; cin >> s;
+        x += s;
+        while(id.size() < x.size()) id.pb(i);
+        if(i + 1 < n) {
+            x += '$';
+            id.pb(-1);
+        }
+    }
 
     vector<int> a = suffix_array(x);
     vector<int> lcp = build_lcp(a, x);
 
     min_queue q;
 
-    add(a[2]);
+    int tot = 0;
+    auto add = [&](int p) {
+        int x = id[p];
+        if(cnt[x] == 0) tot++;
+        cnt[x]++;
+    };
+
+    auto remove = [&](int p) {
+        int x = id[p];
+        cnt[x]--;
+        if(cnt[x] == 0) tot--;
+    };
+
+    add(a[n]);
 
     int ans = 0;
-    for(int j = 3, i = 2; i < x.size(); ++i) {
+    for(int j = n+1, i = n; i < x.size(); ++i) {
         // adiciona quantos precisa
-        while(j < n and tot < 2) {
+        while(j < x.size() and tot < n) {
             add(a[j]);
             if(j-1 >= i) q.push(lcp[j-1]);
             j++;
         } 
-        if(tot == 2) ans = max(ans, q.men());
+        if(tot == n) ans = max(ans, q.men());
 
         if(j >= i + 1) q.pop();
         remove(a[i]);
     }
 
     cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int t; cin >> t;
+
+    while(t--) {
+        _solve();
+    }
+
 
 }
