185815ded95d9a5c4decbb927d04089133e77c97
Lucas Turci <lucas.turci@gmail.com>
2020-06-17 15:58:13 -0300

    Add ImplicitSet (segtree with split and merge operations). NOT TESTED YET

---
+++ Segtree/ImplicitSet/prog.cpp
@@ -0,0 +1,115 @@
+// Implicit multiset of elements between 1 and N (or you can compress them to the range [1, N])
+// See https://codeforces.com/blog/entry/49446
+/*
+	Operations: 
+	1. Add an element to a multiset
+	1. Split one multiset into two multisets (put first k into the first and remaining into the second)
+	2. Merge two sets into one
+
+	Queries:
+	1. Is x in the set? If so, how many of it?
+	2. What is the kth element of the set?
+
+	Complexity is O(Q logN), time and memory
+	Proof: 
+
+	If there were only add and split operations, for every operation you create additional logN nodes. So everything is O(Q logN )
+	Let's call the set of nodes we create S.
+
+	Now, in every merge operation (see code), you visit all the nodes that belong to both segtrees, but don't touch the ones that 
+	are not present in at least one of them. So every visited node is present in both segtrees, and in the merge operation you
+	discard one of them. This is objectively the same as choosing some nodes from S and removing them from the set. 
+	So you never visit more than Q logN nodes in the merge operations
+
+	Note that because of that, you can also add persistence to the merge operation, and the complexity will still remain QlogN time and memory
+	(see https://discuss.codechef.com/t/difval-editorial/68523)
+
+	The proof is a little more complex, because now for every node you visit in S, you discard one but you also add one because of persistence.
+	But, if you look at merges in a sequence of updates (for example: T1 -> merge something -> T2 -> add something -> T3 -> split -> T4... )
+	In a sequence or chain of updates, in every merge operation, you add one node (persistence) and discard **two** (the previous nodes of both trees).
+	And now this is enough to prove that it's still QlogN
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define mid ((l+r) >> 1)
+
+const int N = 5000007;
+
+struct node {
+	int l, r; // children left and right
+	int s; // sum
+};
+
+
+node seg[N];
+int tt;
+
+void new_node() {
+	++sz;
+	seg[sz].l = seg[sz].r = seg[sz].s = 0;
+}
+
+// add x occurences of j
+void add(int & cur, int l, int r, int j, int x) {
+	if(cur == 0) cur = new_node();
+	if(l == r) {
+		seg[cur].s += x;
+	} else {
+		if(j <= mid) add(seg[cur].l, l, mid, j, x);
+		else add(seg[cur].r, mid+1, r, j, x);
+	}
+}
+
+
+inline int sz(int x) {
+	return x == 0? 0 : seg[x].sz;
+}
+
+// split t1 into t1 and t2, putting first k in t1
+void split(int t1, int & t2, int k) {
+	if(!t1) return;
+	
+	t2 = new_node();
+
+	if(sz(seg[t1].l) == k) {
+		swap(seg[t1].r, seg[t2].r);
+	} else if(sz(seg[t1].l) < k) {
+		split(seg[t1].r, seg[t2].r, k - sz(seg[t1].l));
+	} else {
+		swap(seg[t1].r, seg[t2].r);
+		split(seg[t1].l, seg[t2].l, k);
+	}
+	
+	seg[t2].s = seg[t1].s - k;
+	seg[t1].s = k;
+}
+
+// merging t1 and t2 into t1
+void merge(int t1, int t2) {
+	if(!t1 or !t2) return t1? t1 : t2;
+	merge(seg[t1].l, seg[t2].l);
+	merge(seg[t1].r, seg[t2].r);
+	seg[t1].s += seg[t2].s;
+}
+
+// ask for kth
+int kth(int cur, int l, int r, int k) {
+	if(l == r) return l;
+
+	if(sz(seg[cur].l) >= k) return kth(seg[cur].l, l, mid, k);
+	else return kth(seg[cur].r, mid+1, r, k - sz(seg[cur].l));
+}
+
+// counts how many x's
+int find(int cur, int l, int r, int x) {
+	if(!cur or r < x or l > x) return 0;
+	if(l == r) return seg[cur].s;
+	return find(seg[cur].l, l, mid, x) + find(seg[cur].r, mid+1, r, x);
+}
+
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+}
