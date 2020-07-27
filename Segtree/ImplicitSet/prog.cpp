37093372c0649a2264c42499c13ec9ec9d2f66bc
Lucas Turci <lucas.turci@gmail.com>
2020-07-26 23:46:34 -0300

    Fix many things in implicit set. Now it is tested

--- Segtree/ImplicitSet/prog.cpp
+++ Segtree/ImplicitSet/prog.cpp
@@ -3,8 +3,9 @@
 /*
 	Operations: 
 	1. Add an element to a multiset
-	1. Split one multiset into two multisets (put first k into the first and remaining into the second)
-	2. Merge two sets into one
+	2. Split one multiset into two multisets (put first k into the first and remaining into the second)
+	3. Split one multiset into two multisets (put all keys less than k into the first and remaining into the second)
+	4. Merge two sets into one
 
 	Queries:
 	1. Is x in the set? If so, how many of it?
@@ -35,7 +36,7 @@
 using namespace std;
 #define mid ((l+r) >> 1)
 
-const int N = 5000007;
+const int N = 5000007; // I think this is a good N for 10^5
 
 struct node {
 	int l, r; // children left and right
@@ -46,9 +47,10 @@ struct node {
 node seg[N];
 int tt;
 
-void new_node() {
-	++sz;
-	seg[sz].l = seg[sz].r = seg[sz].s = 0;
+int new_node() {
+	++tt;
+	seg[tt].l = seg[tt].r = seg[tt].s = 0;
+	return tt;
 }
 
 // add x occurences of j
@@ -59,12 +61,13 @@ void add(int & cur, int l, int r, int j, int x) {
 	} else {
 		if(j <= mid) add(seg[cur].l, l, mid, j, x);
 		else add(seg[cur].r, mid+1, r, j, x);
+		seg[cur].s = (seg[cur].l? seg[seg[cur].l].s : 0) + (seg[cur].r? seg[seg[cur].r].s : 0);
 	}
 }
 
 
 inline int sz(int x) {
-	return x == 0? 0 : seg[x].sz;
+	return x == 0? 0 : seg[x].s;
 }
 
 // split t1 into t1 and t2, putting first k in t1
@@ -86,9 +89,33 @@ void split(int t1, int & t2, int k) {
 	seg[t1].s = k;
 }
 
+// split t1 into t1 and t2, putting all less than or equal to x into t1
+void split(int t1, int & t2, int l, int r, int x) {
+	if(!t1) return;
+	
+	t2 = new_node();
+
+	if(mid == x) {
+		swap(seg[t1].r, seg[t2].r);
+	} else if(mid < x) {
+		split(seg[t1].r, seg[t2].r, mid+1, r, x);
+	} else {
+		swap(seg[t1].r, seg[t2].r);
+		split(seg[t1].l, seg[t2].l, l, mid, x);
+	}
+
+
+	if(l == r) {
+		if(x < mid) swap(seg[t1].s, seg[t2].s);
+	} else {
+		seg[t1].s = (seg[t1].l? seg[seg[t1].l].s : 0) + (seg[t1].r? seg[seg[t1].r].s : 0);
+		seg[t2].s = (seg[t2].l? seg[seg[t2].l].s : 0) + (seg[t2].r? seg[seg[t2].r].s : 0);		
+	}
+}
+
 // merging t1 and t2 into t1
-void merge(int t1, int t2) {
-	if(!t1 or !t2) return t1? t1 : t2;
+void merge(int & t1, int t2) {
+	if(!t1 or !t2) return void(t1 = (t1? t1 : t2));
 	merge(seg[t1].l, seg[t2].l);
 	merge(seg[t1].r, seg[t2].r);
 	seg[t1].s += seg[t2].s;
